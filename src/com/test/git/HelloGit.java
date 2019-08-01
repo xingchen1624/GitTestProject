7806ea13fbdf53c5e3aafb291fe6c4a3ac20d93b
xingchen1624 <xingchen19890624@163.com>
2019-08-01 20:35:14 +0800

    Initial commit

---
+++ src/com/test/git/HelloGit.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (C), 2012-2019, by xavier chen
+ * FileName: HelloGit
+ * Author:   chen
+ * Date:     2019/8/1 20:33
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.test.git;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈〉
+ *
+ * @author chen
+ * @createDate 2019/8/1
+ * @since 1.0.0
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
